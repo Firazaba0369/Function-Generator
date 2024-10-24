17f950d54187f4a25897e7ee1f3682f2d14fcf62
Firazaba0369 <123702516+Firazaba0369@users.noreply.github.com>
2024-10-23 19:46:04 -0700

    Update and rename LULables.h to LUTs.h

---
+++ LUTs.h
@@ -0,0 +1,14 @@
+/*
+ * LULtables.h
+ *
+ *  Created on: Oct 23, 2024
+ *      Author: firaz
+ */
+
+#ifndef INC_LUTS_H_
+#define INC_LUTS_H_
+
+extern const uint16_t sine[588];
+
+#endif /* INC_LUTS_H_ */
+
