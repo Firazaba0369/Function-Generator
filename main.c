d722d9a3152029a62d5d5c23b33266bdd3a40496
Firazaba0369 <123702516+Firazaba0369@users.noreply.github.com>
2024-10-25 02:00:49 -0700

    Update main.c

--- main.c
+++ main.c
@@ -22,15 +22,29 @@
 #include "timer.h"
 #include "keypad.h"
 #include "LUTs.h"
+#define VOLT_HIGH 3000 //high voltage for duty cycle
+#define VOLT_LOW 0 //low voltage for duty cycle
+#define MAX_DUTY_CYCLE 9 //max duty cycle
+#define MIN_DUTY_CYCLE 1 //min duty cycle
 
 // Global variable to store keypress value
-volatile uint8_t keypress_value = 9; //initialized to 9 for square wave
+volatile uint8_t keypress_val = 9; //initialized to 9 for square wave
 //Global variable to store frequency
 volatile uint16_t freq = 1; //initialized to 1 for 100Hz
+//Global variable to store duty cycle
+volatile uint8_t duty_cycle = 5; //initialize to 50%
+//Global variable for adding duty cycle to square wave
+volatile uint16_t volt_val = 0; //initialize to 0
+//Global variable to detect if square wave
+volatile uint8_t is_square = 1;
 //prototype for interrupt function
 void TIM2_IRQHandler(void);
 //prototype for output waveform function
 void output_waveform(uint8_t keypress);
+//prototype for update frequency function
+void update_freq(uint8_t keypress);
+//prototype for update duty cycle function
+void update_duty_cycle(uint8_t keypress);
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
@@ -50,8 +64,7 @@ int main(void)
   SystemClock_Config();
 
   /* Initialize all configured peripherals */
-
-//  //---------------Configure GPIOA port for ISR time measuring--------------------//
+  //---------------Configure GPIOA port for ISR time measuring--------------------//
   //configure GPIOA clock
   RCC->AHB2ENR   |=  (RCC_AHB2ENR_GPIOAEN);
   //setup MODER for row output
@@ -67,22 +80,37 @@ int main(void)
 
   //initialize DAC, TIM2, and keypad
   DAC_init();
-  TIM2_init((uint16_t)685);
+  TIM2_init();
   keypad_init();
+
   /* Infinite loop */
   while (1)
   {
-
+	  uint8_t keypad = NO_PRESS; //variable to read key-press
+	  while(keypad == NO_PRESS){ //wait for key-press
+		  keypad = keypad_func(); //read key-press
+	  }
+	  while(keypad_func() != NO_PRESS); //wait for key release
+	  keypress_val = keypad; //update global key-press to keypad value
+	  update_freq(keypress_val); //update frequency if needed
+	  //check if waveform is square wave
+	  if (is_square == 1){
+		  update_duty_cycle(keypress_val); //update duty cycle if needed
+	  }
   }
 }
 
 volatile uint16_t lut_index = 0; //index for
 void TIM2_IRQHandler(void){
-	//check for update event flag
-	if (TIM2->SR & TIM_SR_UIF){
+	//check for CC1 flag
+	if (TIM2->SR & TIM_SR_CC1IF){
+		DAC_write(DAC_volt_conv(volt_val)); //add a high or low volt value to square wave
+		TIM2->SR &= ~(TIM_SR_CC1IF); //clear and update CCR1 flag
+	}
+	//check for ARR flag
+	else if (TIM2->SR & TIM_SR_UIF){
 		GPIOA->ODR |= (GPIO_ODR_OD1); //set bit high
-		output_waveform(keypress_value);
-		//DAC_write(DAC_volt_conv(sine[lut_index])); //write the voltage point to the DAC
+		output_waveform(keypress_val); //output according waveform
 		lut_index+=freq; //index by frequency
 		if (lut_index >= 588) {
 			lut_index = 0; // Loop back to the start
@@ -92,6 +120,7 @@ void TIM2_IRQHandler(void){
 	}
 }
 
+
 /**
   * @brief Output Waveform
   * @retval None
@@ -99,18 +128,90 @@ void TIM2_IRQHandler(void){
 void output_waveform(uint8_t keypress){
 	if(keypress == 6){
 		DAC_write(DAC_volt_conv(sine[lut_index])); //output sine wave
+		is_square = 0; //no square wave
 	}
 	else if(keypress == 7){
-			DAC_write(DAC_volt_conv(triangle[lut_index])); //output triangle wave
+		DAC_write(DAC_volt_conv(triangle[lut_index])); //output triangle wave
+		is_square = 0;	//no square wave
 	}
 	else if(keypress == 8){
-			DAC_write(DAC_volt_conv(ramp[lut_index])); //output ramp wave
+		DAC_write(DAC_volt_conv(ramp[lut_index])); //output ramp wave
+		is_square = 0; //no square wave
 	}
 	else if(keypress == 9){
 		DAC_write(DAC_volt_conv(square[lut_index])); //output square wave
+		is_square = 1; //set square wave
 	}
 	else{
 		DAC_write(0);//error
+	}
+}
+
+/**
+  * @brief Update Frequency
+  * @retval None
+  */
+void update_freq(uint8_t keypress){
+	if(keypress == 1){
+		freq = 1; //update frequency to 100Hz
+	}
+	else if(keypress == 2){
+		freq = 2; //update frequency to 200Hz
+	}
+	else if(keypress == 3){
+		freq = 3; //update frequency to 300Hz
+		}
+	else if(keypress == 4){
+		freq = 4; //update frequency to 400Hz
+	}
+	else if(keypress == 5){
+		freq = 5; //update frequency to 500Hz
+	}
+	else{
+		return; //frequency stays the same
+	}
+}
+
+/**
+  * @brief Update Duty Cycle
+  * @retval None
+  */
+void update_duty_cycle(uint8_t keypress){
+	//check if duty cycle needs to be increased by 10%
+	if(keypress == ASTERISK && duty_cycle != MAX_DUTY_CYCLE){
+		TIM2->CCR1 += ARR_VAL / 10 ; //increase CCR time by 10% of ARR
+		duty_cycle += 1; //increase duty cycle by 10%
+
+	    //check if duty cycle is above or below 50%
+		if(duty_cycle > 5){
+			volt_val = VOLT_HIGH; //set voltage high
+		}
+		else{
+			volt_val = VOLT_LOW; //set voltage low
+		}
+	}
+	//check if duty cycle needs to be decreased by 10%
+	else if(keypress == POUND && duty_cycle != MIN_DUTY_CYCLE){
+		TIM2->CCR1 -= ARR_VAL / 10 ;//decrease CCR time by 10% of ARR
+		duty_cycle -= 1; //decrease duty cycle by 10%
+
+		//check if duty cycle is above or below 50%
+		if(duty_cycle > 5){
+			volt_val = VOLT_HIGH; //set voltage high
+		}
+		else{
+			volt_val = VOLT_LOW; //set voltage low
+		}
+	}
+	//check if duty cycle needs to be reset to 50%
+	else if(keypress == 0){
+		//reset to 50% duty cycle
+		TIM2->CCR1 = 0;
+		duty_cycle = 5;
+	}
+	//otherwise do nothing
+	else{
+		return;
 	}
 }
 
