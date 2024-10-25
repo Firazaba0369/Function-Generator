f842b00ff1c13d5ee860396f23700b65b5399389
Firazaba0369 <123702516+Firazaba0369@users.noreply.github.com>
2024-10-25 14:47:22 -0700

    Update LUTs.h

--- LUTs.h
+++ LUTs.h
@@ -9,8 +9,9 @@
 #define INC_LUTS_H_
 
 extern const uint16_t sine[588];
-extern const uint16_t square[588];
+//extern uint16_t* square;
 extern const uint16_t ramp[588];
 extern const uint16_t triangle[588];
+void gen_square_wave(uint16_t *square_array, uint8_t duty);
 
 #endif /* INC_LUTS_H_ */
