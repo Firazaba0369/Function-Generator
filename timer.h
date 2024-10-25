8eab8604537baffc4dd7628f347eca2fd952d638
Firazaba0369 <123702516+Firazaba0369@users.noreply.github.com>
2024-10-25 02:02:11 -0700

    Update timer.h

--- timer.h
+++ timer.h
@@ -7,7 +7,9 @@
 
 #ifndef INC_TIMER_H_
 #define INC_TIMER_H_
+#define ARR_VAL 685 //value of ARR
+#define CCR_VAL 0 //value of CCR*/
 
-void TIM2_init(uint16_t arr_val);
+void TIM2_init(void);
 
 #endif /* INC_TIMER_H_ */
