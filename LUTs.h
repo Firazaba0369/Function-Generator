dacab32e25abbf333bf993a6ef93cf2ec1732646
Firazaba0369 <123702516+Firazaba0369@users.noreply.github.com>
2024-10-28 17:06:44 -0700

    Update LUTs.h

--- LUTs.h
+++ LUTs.h
@@ -8,9 +8,9 @@
 #ifndef INC_LUTS_H_
 #define INC_LUTS_H_
 
-extern const uint16_t sine[588];
-extern const uint16_t ramp[588];
-extern const uint16_t triangle[588];
+extern const uint16_t sine[1720]; //
+extern const uint16_t ramp[1720];
+extern const uint16_t triangle[1720];
 void gen_square_wave(uint16_t *square_array, uint8_t dc);
 
 #endif /* INC_LUTS_H_ */
